5582fe31bac799c3414e03812d33b2c1f5aecf3e
SowmiyaAnbujothi <133943569+SowmiyaAnbujothi@users.noreply.github.com>
2023-05-19 09:51:44 +0530

    Update C3.java

--- src/Git_example/C3.java
+++ src/Git_example/C3.java
@@ -4,7 +4,7 @@ public class C3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is C3 file");
+		System.out.println("This is C3");
 	}
 
 }
